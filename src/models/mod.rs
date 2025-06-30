1dd555c2b8ae6bc08e31e67b769863631680a767
Reece <susifrozee@gmail.com>
2025-06-30 22:33:32 +0100

    Deleted everything so I can start anew

--- src/models/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod document;
-pub mod metadata;
-
-pub use document::{DocumentLine, LineElement};
-pub use metadata::{DocumentMetadata, ImageMetadata};
--- src/ui/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod menus;
-pub mod status;
-
-pub use menus::*;
-pub use status::*;
